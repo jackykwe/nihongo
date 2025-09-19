18797f05d8d3ab849449c851173c57bb49435ede
Jacky Kung <40329372+jackykwe@users.noreply.github.com>
2025-09-19 15:35:38 +0800

    Migrate dev-server to separate repository

--- dev-server/backend/src/utils.rs
+++
@@ -1,3 +0,0 @@
-pub(crate) mod errors;
-pub(crate) mod logging;
-pub(crate) mod top_level_listener;
--- dev-server/backend/src/utils/errors.rs
+++
@@ -1,8 +0,0 @@
-use axum::http::StatusCode;
-use tracing::{instrument, warn};
-
-#[instrument]
-pub(crate) async fn global_fallback() -> StatusCode {
-    warn!("You hit the global fallback function.");
-    StatusCode::NOT_FOUND
-}
